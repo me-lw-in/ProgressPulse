59f6232364981f09d5c202b05eebf7ec36e9aa82
me-lw-in <melwinmanish30@gmail.com>
2024-05-26 19:22:07 +0530

    updated settings

--- AttendanceProject/settings.py
+++ AttendanceProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-66h6a1u&cg-rjb+z^7s+j*i0yz+6i-wglh8(65&lqrvx*8=&jc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
